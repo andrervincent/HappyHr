76c013ea3fc875fbb1f57a3c7292332e5b0af84b
André Vincent <andrevncnt7@gmail.com>
2020-03-26 00:08:30 -0500

    sets up project folder

---
+++ FinalProj/FinalProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProj
+//
+//  Created by Andre Vincent on 3/25/20.
+//  Copyright © 2020 Andre Vincent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
