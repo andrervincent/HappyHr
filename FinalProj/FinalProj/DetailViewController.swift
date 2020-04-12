2004b389374c196cc0fc9e3664493d66eb31d249
André Vincent <andrevncnt7@gmail.com>
2020-04-12 00:19:08 -0500

    Updates Files with Authentication, SignUp, and Detail Views with respective screens.

---
+++ FinalProj/FinalProj/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  FinalProj
+//
+//  Created by Andre Vincent on 4/11/20.
+//  Copyright © 2020 Andre Vincent. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageField: UIImageView!
+    @IBOutlet weak var locationName: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
